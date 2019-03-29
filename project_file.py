e0537912ae84e783dc83068068a440ff72a7263a
cklineuhs <46720459+cklineuhs@users.noreply.github.com>
2019-03-29 14:20:42 -0400

    Create project_file.py
    Creating the main file to branch from

---
+++ project_file.py
@@ -0,0 +1,14 @@
+    
+# *********************************************************************************************************
+# Class: IT-512 Spring 2019
+# Author: 
+# Due Date: 
+# Software/Program Reference Number: Final Project
+# Software/Program Brief Description: 
+#
+# Ver No.    Name(Initials)  Date           Description
+# =======    ==============  ===========    ===========
+# 1.0        CK              2019/29/03     First Version
+#
+# *********************************************************************************************************
+# Variable Initialization
