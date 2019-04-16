06a26deede705658884696ad57b52df0ac5f058b
hngkong <hngkong@gmail.com>
2019-04-16 19:48:32 -0400

    Update project_file.py
    Basic layout of code

--- project_file.py
+++ project_file.py
@@ -13,4 +13,33 @@
 # *********************************************************************************************************
 # Variable Initialization
 
-Testing a change
+class Board:
+    player1Count = 0
+    player2Count = 0
+    player1Deck
+    player2Deck
+    player1Score = 0
+    player2Score = 0
+    
+    def self.dealCard(#*somesuch here*#):
+        # gen a random number, add to the next player's count total?
+        
+class Deck:
+    card1 = 0
+    card2 = 0
+    card3 = 0
+    card4 = 0
+    
+    def self.GenerateCards():
+        card1 = random()
+        card2 = random()
+        card3 = random()
+        card4 = random()
+        
+    def self.UseCard():
+        card = 0
+        
+def main():
+    Player1Deck = Deck()
+    Player2Deck = Deck()
+    BoardState = Board()
